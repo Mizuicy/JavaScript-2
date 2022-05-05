64787d6f9e58624a9ab452c2298bfcb19329addc
Mizuicy <102737167+Mizuicy@users.noreply.github.com>
2022-05-05 10:55:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function nome (a) {
+    console.log(a) 
+    return 
+}
+nome("Gustavo")
+
+//function: cria uma função
+//toda função tem um nome 
+//parãmetros = ()
+//some 2 numeros
+
+function soma (a,b) {
+    return console.log(a + b)
+    
+}
+
+soma(342,234)
+
+//mutiplicar
+
+function mutiplicar (a,b) {
+    return console.log(a * b)
+    
+}
+
+mutiplicar(23,21)
+
+//media
+
+function media(a,b,c,d){
+    let res = (a+b+c+d)/4
+    console.log(res)
+    return
+
+}
+
+media(54, 57,45,34)
